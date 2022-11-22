394125d79c86d0a7910beef77acd09d0737a05ba
henriquesc10 <henriquesc10@hotmail.com>
2022-11-22 16:26:47 -0300

    Service layer, component registration

---
+++ src/main/java/com/henriqusc/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.henriqusc.course.services;
+
+import com.henriqusc.course.entities.User;
+import com.henriqusc.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.get();
+    }
+}
